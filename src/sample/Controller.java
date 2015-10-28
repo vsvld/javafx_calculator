c4862c130b2a1f52d82977a1a40f88d66d640249
vsvld <v.alyokhin@gmail.com>
2015-10-28 10:16:43 +0200

    fxml elements in controller, making unified method for buttons' actions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,7 +2,88 @@ package sample;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class Controller {
-    public StringProperty result = new SimpleStringProperty();
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+//    public StringProperty result = new SimpleStringProperty("0");
+
+    // TODO variables declaration in one line if possible
+
+    @FXML
+    private TextField textField;
+
+    // digits buttons
+    @FXML
+    private Button zeroButton;
+    @FXML
+    private Button oneButton;
+    @FXML
+    private Button twoButton;
+    @FXML
+    private Button threeButton;
+    @FXML
+    private Button fourButton;
+    @FXML
+    private Button fiveButton;
+    @FXML
+    private Button sixButton;
+    @FXML
+    private Button sevenButton;
+    @FXML
+    private Button eightButton;
+    @FXML
+    private Button nineButton;
+
+    // actions with 2 numbers buttons
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button subtractButton;
+    @FXML
+    private Button multiplyButton;
+    @FXML
+    private Button divideButton;
+
+    // actions with 1 number buttons
+    @FXML
+    private Button invertButton;
+    @FXML
+    private Button commaButton;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button powerHalfButton;
+    @FXML
+    private Button equalsButton;
+
+    private double firstNumber, secondNumber, result;
+
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+        Button btn = (Button) e.getSource();
+
+        switch (btn.getText()) {
+            case "C":
+                firstNumber = 0;
+                showNumber(firstNumber);
+                break;
+        }
+    }
+
+    private void showNumber(double number) {
+        textField.setText(Double.toString(number));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
