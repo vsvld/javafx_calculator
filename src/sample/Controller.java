d1bb1674127c09df53b6da5a1e3909670ad2aa68
vsvld <v.alyokhin@gmail.com>
2015-11-06 01:43:27 +0200

    all working except special button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,6 @@
 package sample;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -9,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -64,26 +64,115 @@ public class Controller implements Initializable {
     @FXML
     private Button equalsButton;
 
-    private double firstNumber, secondNumber, result;
+    private DoubleProperty activeNumber = new SimpleDoubleProperty();
+    private DoubleProperty result = new SimpleDoubleProperty();
+    private BooleanProperty isDecimal = new SimpleBooleanProperty();
+    private BooleanProperty withPoint = new SimpleBooleanProperty();
+    private StringProperty savedOperation = new SimpleStringProperty();
 
     @FXML
     private void handleButtonAction(ActionEvent e) {
+
         Button btn = (Button) e.getSource();
 
         switch (btn.getText()) {
             case "C":
-                firstNumber = 0;
-                showNumber(firstNumber);
+                activeNumber.setValue(0);
+                result.setValue(0);
+                showNumber(activeNumber);
+                break;
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                if (isDecimal.getValue() && !withPoint.getValue()) {
+                    activeNumber.setValue(Double.parseDouble(fmt(activeNumber.getValue()) + '.' + btn.getText()));
+                    withPoint.setValue(true);
+                }
+                else if (activeNumber.getValue() == 0) {
+                    activeNumber.setValue(Integer.parseInt(btn.getText()));
+                }
+                else {
+                    activeNumber.setValue(Double.parseDouble(fmt(activeNumber.getValue()) + btn.getText()));
+                }
+                showNumber(activeNumber);
+                break;
+            case ",":
+                if (!isDecimal.getValue()) isDecimal.setValue(true);
+                break;
+            case "+/-":
+                activeNumber.setValue(activeNumber.getValue() * -1);
+                showNumber(activeNumber);
+                break;
+            case "/":
+            case "*":
+            case "-":
+            case "+":
+                if (!Objects.equals(savedOperation.getValue(), "my_null")) {
+                    calculateResult();
+                    showNumber(result);
+                } else {
+                    result.setValue(activeNumber.getValue());
+                }
+                savedOperation.setValue(btn.getText());
+                activeNumber.setValue(0);
+                break;
+            case "=":
+                calculateResult();
+                showNumber(result);
+                activeNumber.setValue(0);
+                result.setValue(0);
                 break;
         }
+//        System.out.println("active");
+//        System.out.println(activeNumber.getValue());
+//        System.out.println("result");
+//        System.out.println(result.getValue());
+//        System.out.println(savedOperation.getValue());
     }
 
-    private void showNumber(double number) {
-        textField.setText(Double.toString(number));
+    private void showNumber(DoubleProperty number) {
+        textField.setText(fmt(number.getValue()));
+    }
+
+    private void calculateResult() {
+        switch (savedOperation.getValue()) {
+            case "/":
+                result.setValue(result.getValue() / activeNumber.getValue());
+                break;
+            case "*":
+                result.setValue(result.getValue() * activeNumber.getValue());
+                break;
+            case "-":
+                result.setValue(result.getValue() - activeNumber.getValue());
+                break;
+            case "+":
+                result.setValue(result.getValue() + activeNumber.getValue());
+                break;
+        }
+        savedOperation.setValue("my_null");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        activeNumber.setValue(0);
+        result.setValue(0);
+        isDecimal.setValue(false);
+        withPoint.setValue(false);
+        savedOperation.setValue("my_null");
+        showNumber(activeNumber);
+    }
 
+    public static String fmt(double d) {
+        if (d == (long) d)
+            return String.format("%d", (long) d);
+        else
+            return String.format("%s", d);
     }
 }
